74df6b828a008047d45528a0d674bcbec59d275a
Danielle Costa <d.mariano.costa@gmail.com>
2025-06-11 20:13:00 -0300

    🎉 feat(ex05): implement debounce function

---
+++ exercicios-2/ex05.js
@@ -0,0 +1,13 @@
+// Exercício 5 - Crie function debounce(fn, delay) que receba uma função fn e um delay em ms, retornando uma nova função que só executa fn se não for chamada novamente dentro do intervalo.
+
+function debounce(fn, delay) {
+    let timeoutId
+
+    return function (... args) {
+        clearTimeout(timeoutId)
+        timeoutId = setTimeout(() => {
+            fn.apply(this, args)
+        }, delay)
+    }
+}
+
